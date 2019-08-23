67916a3838c0bb062e415afdbaaafc9064ee9c7a
AV3110 <akshansh3110@gmail.com>
2019-08-23 22:59:06 +0530

    Initial commit

---
+++ app/src/main/java/com/lightsabers/prototype_1/WeatherSwitches.java
@@ -0,0 +1,16 @@
+package com.lightsabers.prototype_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class WeatherSwitches extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_switches);
+
+    }
+}
